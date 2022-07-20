dc44936c01e7985bab95515a770fe0eb5e1ec020
Ashley Wong <tofudragon333@gmail.com>
2022-07-19 20:58:16 -0400

    commit tree 6f029b2ff166e996b0d7b6544a77d8848274b7e6

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+    #GET /birds
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    def show
+        bird = Bird.find(params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "Bird not found"}, status: :not_found
+        end
+    end
+end
